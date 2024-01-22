909c58a51cf6840c929f1377bd083075de1f8bfe
Carlos Arias <43482712+carlosaariasl@users.noreply.github.com>
2024-01-22 09:17:00 -0600

    Add files via upload

---
+++ hola_mundo.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int edad = 0;
+float precio = 4.5;
+char letra = 'i';
+float resultado = 5;
+
+void main()
+{
+    suma();
+}
+
+float suma(){
+    // instrucciones de programación para calcular la suma
+    return resultado;
+}
+
+float resta(){
+    // instrucciones de programación para calcular la resta
+    return resultado;
+}
+
+float multiplicacion(){
+    // instrucciones de programación para calcular la multiplicacion
+    return resultado;
+}
